b134bf2385cdc8d05ee96aed8a9f8df709c7c03b
A1exFox <alex.fox.personal@gmail.com>
2024-09-26 18:09:28 +0300

    #1.04 [t:TSingleton], [c:Registry], [c:App]

--- www/public/index.php
+++ www/public/index.php
@@ -4,3 +4,7 @@ if (PHP_MAJOR_VERSION < 8)
     die('PHP version has to be >= 8');
 
 require_once dirname(__DIR__) . '/config/init.php';
+
+new \wfm\App();
+
+var_dump(\wfm\App::$app->getProperties());
