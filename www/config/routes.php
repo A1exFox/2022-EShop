f6963af77feca7a30b591ee67498b1e9ca9ed38f
A1exFox <alex.fox.personal@gmail.com>
2024-09-27 14:20:38 +0300

    #1.07 [c:Router] part 2

--- www/config/routes.php
+++ www/config/routes.php
@@ -5,5 +5,5 @@ use wfm\Router;
 Router::add('^admin/?$', ['controller' => 'Main', 'action' => 'index', 'admin_prefix' => 'admin']);
 Router::add('^admin/(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['admin_prefix' => 'admin']);
 
-Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^$', ['controller' => 'Main']);
 Router::add('^(?P<controller>[a-z-]+)/(?<action>[a-z-]+)/?$');
--- www/vendor/wfm/Router.php
+++ www/vendor/wfm/Router.php
@@ -20,5 +20,56 @@ class Router
     }
     public static function dispatch($url)
     {
+        if (self::matchRoute($url)) {
+            echo 'OK';
+        } else {
+            echo 'NO';
+        }
     }
+    public static function matchRoute($url): bool
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#i", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                if (empty($route['action']))
+                    $route['action'] = 'index';
+                if (!isset($route['admin_prefix']))
+                    $route['admin_prefix'] = '';
+                else
+                    $route['admin_prefix'] .= '\\';
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                debug($route);
+                return true;
+            }
+        }
+        return false;
+    }
+    protected static function upperCamelCase($name): string
+    {
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
+
+    protected static function loewCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
