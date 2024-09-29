a83b9fa701b288cf088d795da4a95499fbcf0906
A1exFox <alex.fox.personal@gmail.com>
2024-09-29 17:58:05 +0300

    #1.13 [c:Db], [c:Model]

--- www/app/controllers/MainController.php
+++ www/app/controllers/MainController.php
@@ -2,13 +2,18 @@
 
 namespace app\controllers;
 
+use app\models\Main;
 use wfm\Controller;
+
+/** @property Main $model */
 
 class MainController extends Controller
 {
     public function indexAction()
     {
         $this->setMeta('Index page', 'Description index page', 'Keywords index page');
+        $names = $this->model->get_names();
+        $this->set(compact('names'));
     }
 }
 
--- www/app/views/Main/index.php
+++ www/app/views/Main/index.php
@@ -1,1 +1,6 @@
 <h1>Hello main index</h1>
+<?php if (!empty($names)): ?>
+    <?php foreach ($names as $name): ?>
+        <?php debug($name['name']); ?>
+    <?php endforeach; ?>
+<?php endif; ?>
