e3a4ce91f6280fed73b46b81954db6bbd48db007
A1exFox <alex.fox.personal@gmail.com>
2024-09-30 20:33:10 +0300

    #2.02 Set E-Shop layout

--- www/app/views/layouts/ishop.php
+++ www/app/views/layouts/ishop.php
@@ -3,5 +3,23 @@ use wfm\View;
 /** @var $this View */
 ?>
 <?php $this->getPart('parts/header'); ?>
+
 <?= $this->content ?>
+
 <?php $this->getPart('parts/footer'); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
