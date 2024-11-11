163d8141fc796ec1ad694616e8a83fbb64642800
A1exFox <alex.fox.personal@gmail.com>
2024-11-11 14:53:41 +0300

    #2.39 Checkout part 4

--- www/config/params.php
+++ www/config/params.php
@@ -4,4 +4,11 @@ return [
     'admin_email' => 'admin@new-ishop.loc',
     'site_name' => 'E-Shop',
     'pagination' => 3,
+    'smtp_host' => 'smtp.mailtrap.io',
+    'smtp_auth' => true,
+    'smtp_port' => 2525,
+    'smtp_username' => '851',
+    'smtp_password' => '91ed',
+    'smtp_secure' => null,
+    'smtp_from_email' => '722@inbox.mailtrap.io',
 ];
